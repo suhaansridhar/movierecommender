203ee8efb709d4299f63d560eac54cdadae05154
suhaan sridhar <suhaansridhar@suhaans-MacBook-Pro.local>
2025-05-02 19:55:39 +0530

    initial commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,10 @@
+export default function NavBar(){
+    return(
+        <div className="navbar">
+            <div className="navbar--container">
+                <h2>Movie Recommender</h2>
+                <p>sign out</p>
+            </div>
+        </div>
+    )
+}
